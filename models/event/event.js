503e535c190e529eb33455be26de6cef86a85074
Travis Duncombe <travisduncombe18@gmail.com>
2015-02-16 11:58:48 -0700

    Port Event and showtime, update/move ticket, update seat. remove old files.

--- models/event/event.js
+++ models/event/event.js
@@ -1,3 +1,17 @@
-module.exports = function(Event) {
+module.exports = function (Event) {
 
+  Event.observe('before save', function checkDate(context, next) {
+    if (context.instance) {
+      if (!context.instance.date) {
+        context.instance.date = new Date(Date.now());
+      }
+    }
+    else {
+      if (context.data.date) {
+        context.data.date = new Date(Date.now());
+      }
+    }
+    next();
+
+  });
 };
