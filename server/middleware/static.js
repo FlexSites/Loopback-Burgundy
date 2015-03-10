3a8c7ec0404ac707b2520134340b0b1495e64329
Seth Tippetts <sethtippetts@gmail.com>
2015-03-10 17:30:37 -0600

    Local static file serving

--- server/middleware/static.js
+++ server/middleware/static.js
@@ -1,45 +1,26 @@
 var httpProxy = require('http-proxy');
-var loopback = require('loopback');
-var Promise = Promise || require('bluebird');
 
 module.exports = function(){
+
+  var env = process.env.NODE_ENV || 'local';
+  var isLocal = env === 'local';
   var proxy = httpProxy.createProxyServer({
     target: 'http://flex-sites.s3-website-us-west-2.amazonaws.com'
-  })
-    , abbrMap = {}
-    , Site = loopback.findModel('Site');
+  });
 
   return function(req,res,next){
-    var host = req.headers.host;
-
-    getAbbr(host)
-      .then(function(abbr){
-        req.url = '/'+ abbr + req.url;
-        req.headers.host = 'flex-sites.s3-website-us-west-2.amazonaws.com';
-        proxy.web(req,res,{});
-        proxy.on('error', function(e) {
-          next(e);
-        });
-      }, function(err){
-        next(err);
-      });
-  };
-
-  function getAbbr(host){
-    if(abbrMap[host]){
-      return abbrMap[host];
+    var abbr = req.abbr;
+    if(isLocal){
+      abbr += '/public';
     }
-    return abbrMap[host] = new Promise(function(resolve, reject){
-      Site.findOne({
-        where: {
-          host: /^(local|test|www)?\.?(.*)$/.exec(host)[2]
-        }
-      }, function(err, site) {
-        if(err || !site){
-          return reject(err);
-        }
-        resolve(site.abbr);
-      });
+    req.url = '/'+ abbr + req.url;
+    if(isLocal){
+      return next();
+    }
+    req.headers.host = 'flex-sites.s3-website-us-west-2.amazonaws.com';
+    proxy.web(req,res,{});
+    proxy.on('error', function(e) {
+      next(e);
     });
-  }
+  };
 };
