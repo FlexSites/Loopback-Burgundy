c1609a977d9ce50ca233aefdca4d444eb45415e5
Seth Tippetts <sethtippetts@gmail.com>
2015-03-09 20:42:00 -0600

    Static being proxied to S3 now.

--- server/middleware/static.js
+++ server/middleware/static.js
@@ -1,13 +1,42 @@
 var httpProxy = require('http-proxy');
+var loopback = require('loopback');
+var Promise = Promise || require('bluebird');
 
 module.exports = function(){
   var proxy = httpProxy.createProxyServer({
     target: 'http://flex-sites.s3-website-us-west-2.amazonaws.com'
-  });
+  })
+    , abbrMap = {}
+    , Site = loopback.findModel('Site');
 
   return function(req,res,next){
-    req.headers.host = 'flex-sites.s3-website-us-west-2.amazonaws.com';
-    console.log('request headers', req.headers);
-    proxy.web(req,res,{});
+    var host = req.headers.host;
+
+    getAbbr(host)
+      .then(function(abbr){
+        req.url = '/'+ abbr + req.url;
+        req.headers.host = 'flex-sites.s3-website-us-west-2.amazonaws.com';
+        proxy.web(req,res,{});
+      }, function(err){
+        console.log('Error getting abbreviation for static site: %s', host, err);
+      });
   };
+
+  function getAbbr(host){
+    if(abbrMap[host]){
+      return abbrMap[host];
+    }
+    return abbrMap[host] = new Promise(function(resolve, reject){
+      Site.findOne({
+        where: {
+          host: /^(local|test|www)?\.?(.*)$/.exec(host)[2]
+        }
+      }, function(err, site) {
+        if(err || !site){
+          return reject(err);
+        }
+        resolve(site.abbr);
+      });
+    });
+  }
 };
