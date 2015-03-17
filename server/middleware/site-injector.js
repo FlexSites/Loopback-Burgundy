a183d446a27d59e85b82ed3c1b539dad594c0bd6
Seth Tippetts <sethtippetts@gmail.com>
2015-03-17 17:20:22 -0600

    Cleaning up site-injector logic. Fixing "query all" functionality.

--- server/middleware/site-injector.js
+++ server/middleware/site-injector.js
@@ -1,35 +1,32 @@
 var loopback = require('loopback');
 
 module.exports = function(){
-  var hostMap = {}
-    , idMap = {}
-    , Site = loopback.findModel('Site');
-
+  var siteMap = { host: {}, id: {} }
+    , find = loopback.findModel('Site').findOne;
+    
   return function(req,res,next){
     
     var siteId = req.get('X-FlexSite')
-      , origin = req.flex.origin
-      , query = {where:{}}
+      , type = siteId?'id':'host'
+      , value = siteId || req.flex.origin
       , fn = setSite.bind(loopback.getCurrentContext(), next);
 
-    if(origin && !siteId) {
-      if(hostMap[origin]){
-        return fn(null, hostMap[origin]);
-      }
-      query.where.host = origin;
+    if(!value){
+      return next();
     }
-    else if(siteId){
-      if(idMap[siteId]){
-        return fn(null, idMap[siteId])
-      }
-      query.where.id = siteId;
+    var site = siteMap[type][value];
+    if(site){
+      fn(null, site);
+    } else {
+      var query = { where: {} };
+      query[type] = value;
+      find(query, fn);
     }
-    Site.findOne(query, fn);
   };
 
   function setSite(next, err, site){
     if (!err && site) {
-      this.set('site', hostMap[site.host] = idMap[site.id] = {host: site.host, id:site.id, abbr: site.abbr});
+      this.set('site', siteMap.host[site.host] = siteMap.id[site.id] = {host: site.host, id:site.id, abbr: site.abbr});
     }
     next(err);
   }
