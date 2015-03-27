ec6c392be9c50dfb804516eb3fd27021cd643d52
Seth Tippetts <sethtippetts@gmail.com>
2015-03-26 22:51:15 -0600

    Fixing S3 proxy

--- server/middleware/static-proxy.js
+++ server/middleware/static-proxy.js
@@ -1,38 +1,33 @@
 var httpProxy = require('http-proxy')
-  , loopback = require('loopback')
-  , glob = require('glob');
+  , loopback = require('loopback');
 
 module.exports = function(){
 
   var bucket = process.env.S3_BUCKET
     , proxy = httpProxy.createProxyServer({
+    changeOrigin: true,
     target: bucket
   });
 
   return function(req,res,next){
     var ctx = loopback.getCurrentContext()
-      , site = ctx.get('site')
-      , err;
+      , site = ctx.get('site');
 
     if(!site || !site.abbr){
       return next(new Error('Unrecognized static site.'));
     }
-    var abbr = site.abbr;
-    if(!bucket){
-      if(req.originalUrl === '/'){
-        var idx = glob.sync('/www/sites/' + abbr + '/public/index-*.html');
-        if(idx && idx[0]){
-          req.url = '/'+idx[0].split('/').pop();
-        }
-      } else if(!~req.originalUrl.indexOf('.')){
-        return res.redirect('http://' + req.get('host') + '/#' + req.originalUrl, 301);
-      }
-      req.url = '/' + abbr + '/public' + req.url;
 
+    if(req.originalUrl === '/'){
+      req.url = '/index.html';
+    }
+    else if(!~req.originalUrl.indexOf('.')){
+      return res.redirect(301, 'http://' + req.get('host') + '/#' + req.originalUrl);
+    }
+
+    req.url = '/' + site.abbr + '/public' + req.url;
+    if(!bucket){
       return next();
     }
-    req.url = '/'+ abbr + req.url;
-    req.headers.host = bucket;
     proxy.web(req,res,{});
     proxy.on('error', next);
   };
