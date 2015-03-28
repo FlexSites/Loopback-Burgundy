25fe647844c7e2e84a6858688694ea8207b1d8c9
Travis Duncombe <travisduncombe18@gmail.com>
2015-03-28 13:48:39 -0600

    message.js is fully functioning, gets the contact email from the site. Updated the template in MailService.

--- common/models/message/message.js
+++ common/models/message/message.js
@@ -3,41 +3,48 @@ var MailService = require('../../../server/services/MailService.js');
 
 module.exports = function (Message) {
 
-  Message.observe('before save', function (ctx, next) {
-    var context = loopback.getCurrentContext()
-    , site = context.get('site');
+  Message.observe('before save', function (context, next) {
+    var ctx = loopback.getCurrentContext()
+    , site = ctx.get('site')
+    , Site = loopback.findModel('Site');
 
     if(site)
     {
-      console.log("ctx: " + JSON.stringify(ctx))
-
-      if(ctx.instance){
-
-        console.log("instance: " + JSON.stringify(ctx.instance))
-        var contact = {
-          name: ctx.instance.name,
-          toEmail: ctx.instance.toEmail,
-          subject: ctx.instance.subject,
-          body: ctx.instance.body
-
-        }
+      if(context.instance){
+        Site.findById(site.id, function(err,obj){
+          context.instance.toEmail = obj.contact.email;
+          context.instance.subject = 'New Message from ' + context.instance.name;
+          context.instance.type='contact';
+          context.instance.fromEmail='Comedian.io <contact@comedian.io>';
+          var data = {
+            name: context.instance.name,
+            email: context.instance.email,
+            phone: context.instance.phone,
+            title: context.instance.subject,
+            message: context.instance.body,
+            host: site.host
+          }
+          MailService.contactTemplate(data, function(err,html){
+            if(html){
+               MailService.send(context.instance.toEmail,context.instance.fromEmail
+                ,context.instance.subject,html,function(err,status){
+                 console.log('send mail responded: ' 
+                  + JSON.stringify({message: err||status.message}));
+               });
+            }
+            else{
+              console.log('Template Failed to Build, email wasn\'t sent: ' 
+                + JSON.stringify(context.instance))
+            }
+          });
+          next();
+        })
       }
-
-        // var sendToEmail = site.contact.email;
-        // console.log('send and email to: ' + sendToEmail);
-        console.log('context: ' + JSON.stringify(context));
-        console.log('Site: ' + JSON.stringify(site));
-        //if model is contactMessage use contacttemplate, etc.
-        // MailService.contactTemplate(contact,function(err, results){
-        //   var html = results.html;
-        //   console.log('HTML: ' + html)
-        // });
     }
     else
     {
-        console.log('No Site in context...');
+      console.log('No Site in context for message.');
+      next();
     }
-    next();
-
   });
 };
--- server/services/MailService.js
+++ server/services/MailService.js
@@ -6,26 +6,31 @@ var path = require('path');
 var fs = require('fs');
 
 
+
 module.exports = {
   contactTemplate: function(contact,fn){
-    fs.readFile('/www/sites/global/public/html/email.html','utf8', function (err,data) {
+    fs.readFile('/www/global/html/email.html','utf8', function (err,data) {
       fn(err,hogan.compile(data).render(contact));
     });
   },
   send: function(to,from,subject,body,fn){
+    if(process.env.MAILGUN_API_KEY){
+      var mailgun = new Mailgun({apiKey: process.env.MAILGUN_API_KEY
+        , domain: 'comedian.io'});
+    
+      var data = {
+        from: from,
+        to: to,
+        subject: subject,
+        html: body
+      };
 
-  var mailgun = new Mailgun({apiKey: process.env.{MAILGUN_API_KEY}, domain: 'comedian.io'});
-
-    var data = {
-      from: from,
-      to: to,
-      subject: subject,
-      html: body
-    };
-
-    mailgun.messages().send(data, function (error, body) {
-      console.log('Mailgun',body);
-      fn(error,{message: 'Successfully sent message'});
-    });
+      mailgun.messages().send(data, function (error, body) {
+        fn(error,{message: 'Successfully sent message'});
+      });
+    }
+    else {
+      console.log('Missing Mailgun API Key');
+    }
   }
 };
