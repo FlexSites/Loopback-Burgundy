98a432e1af19e53c1d783fa4479e6bacb408e383
Seth Tippetts <sethtippetts@gmail.com>
2015-03-30 22:45:47 -0600

    Added `FlexUser` resource to provide custom ACLs to User (User resource deprecated until we can figure out how to reverse this)

--- server/boot/authentication.js
+++ server/boot/authentication.js
@@ -1,11 +1,4 @@
 module.exports = function enableAuthentication(server) {
   // enable authentication
   server.enableAuth();
-
-  var User = server.models.User;
-  var Site = server.models.Site;
-   
-  Site.hasAndBelongsToMany(User);
-  User.hasAndBelongsToMany(Site);
-
 };
