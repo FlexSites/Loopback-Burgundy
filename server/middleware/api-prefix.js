e7b07b027b0f2835b0e844300c27b5f811b8b0bd
Seth Tippetts <sethtippetts@gmail.com>
2015-04-23 23:15:36 -0600

    Removing the api prefix (as Sex Panther handles all non-data resources now)

--- server/middleware/api-prefix.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(){
-  return function(req,res,next){
-    if(req.flex.isAPI && !req.flex.isCDN){
-      req.url = '/api' + req.url;
-      res.set('Access-Control-Allow-Headers', 'X-FlexSite');
-    }
-    next();
-  };
-};
--- server/middleware/normalize-host.js
+++ server/middleware/normalize-host.js
@@ -13,26 +13,20 @@ module.exports = function(){
       req.flex.origin = /^(?:https?:\/\/)(.*)$/.exec(origin)[1];
     }
 
+    if(req.method === 'OPTIONS'){
+      res.set('Access-Control-Allow-Headers', 'X-FlexSite');
+    }
+
     if(process.env.NODE_ENV !== 'prod'){
-      if(req.flex.host === '127.0.0.1'){
-        req.flex.host = 'api.flexhub.io';
-        req.flex.isAPI = true;
-      }
       req.flex.host = removePrefix(req.flex.host);
       if(origin){
         req.flex.origin = removePrefix(req.flex.origin);
       }
     }
-    req.flex.isAPI = req.flex.host === 'api.flexhub.io';
-    req.flex.isCDN = req.flex.host === 'cdn.flexsites.io';
-    req.flex.isAdmin = req.flex.host === 'admin.flexsites.io';
     next();
   };
 
   function removePrefix(url){
-    if(/^(local|test)/.test(url)){
-      url = /^(?:local|test)\.?(.*)$/.exec(url)[1];
-    }
-    return url;
+    return /^(?:local|test)\.?(.*)$/.exec(url)[1];
   }
 };
