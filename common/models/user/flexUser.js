7ceb2fac9db3aca2b77e3c8fb1734f90d8e6c326
Seth Tippetts <sethtippetts@gmail.com>
2015-04-25 00:45:26 -0600

    Fixing image hash for gravatar

--- common/models/user/flexUser.js
+++ common/models/user/flexUser.js
@@ -1,11 +1,18 @@
-var crypto = require('crypto');
+var crypto = require('crypto')
+  , selectn = require('selectn');
 
 module.exports = function(FlexUser){
   FlexUser.afterRemote('*', function (ctx, user, next) {
-    if(ctx.req.flex.isAdmin) return next();
-
+    var email = selectn('result.email')(ctx) || selectn('result.user.email')(ctx) || user.email;
+    console.log('method name', ctx.method.name, 'result', selectn('result.user')(ctx), 'user', JSON.stringify(user.user));
+    if(!email) return next();
     if(!Array.isArray(ctx.result)){
-      ctx.result.image = getImage(ctx.result.email);
+      var image = getImage(user.email);
+      if(/^log/.test(ctx.method.name)){
+        ctx.result.user.image = image;
+      } else {
+        ctx.result.image = image;
+      }
       return next();
     }
     ctx.result = ctx.result.map(function(result, i){
@@ -17,5 +24,9 @@ module.exports = function(FlexUser){
 };
 
 function getImage(email){
-  return 'http://www.gravatar.com/avatar/' + crypto.createHash('md5').update(email).digest('hex');
+  var image = 'http://www.gravatar.com/avatar/';
+  if(email){
+     image += crypto.createHash('md5').update(email).digest('hex');
+  }
+  return image;
 }
