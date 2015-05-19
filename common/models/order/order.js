256e3fae74441e3ac578ead1049a29027e7b2f11
Seth Tippetts <sethtippetts@gmail.com>
2015-05-18 21:00:53 -0600

    Adding support for single transactions, and scaffolding for recurring payments

--- common/models/order/order.js
+++ common/models/order/order.js
@@ -2,6 +2,7 @@ var loopback = require('loopback');
 var aws = require('aws-sdk');
 var crypto = require('crypto');
 var PaymentService = require('../../../server/services/PaymentService');
+var selectn = require('selectn');
 
 module.exports = function(Order){
 
@@ -13,15 +14,24 @@ module.exports = function(Order){
     PaymentService.createTransaction(ctx.instance.payment_method_nonce, ctx.instance.amount, next);
   });
 
-  Order.token = function(name, type, cb) {
-    PaymentService.generateToken(null, cb);
+  Order.token = function(name, type, userId, cb) {
+    PaymentService.generateToken({
+      customerId: userId
+    }, cb);
   };
 
   Order.remoteMethod(
     'token',
     {
       http: {path: '/token', verb: 'get'},
-      accepts: [{arg: 'name', type: 'string' }, {arg: 'type', type: 'string' }],
+      accepts: [
+        {arg: 'name', type: 'string' },
+        {arg: 'type', type: 'string' },
+        {arg: 'userId', type: 'string', required: true, http: function(ctx){
+          var userId = selectn('req.accessToken.userId', ctx);
+          if(userId) return userId;
+        }}
+      ],
       returns: {arg: 'token', type: 'string'}
     }
   );
--- server/services/PaymentService.js
+++ server/services/PaymentService.js
@@ -7,6 +7,10 @@ var gateway = braintree.connect({
     privateKey:   '08e99b95f6221e4a68b4fa778b8c87c7'
 });
 
+// gateway.customer.find('5518b349a75a2abb810ebfc3', function(err, customer){
+//   console.log('CUSTOMER', err, customer);
+// })
+
 module.exports = {
   generateToken: function(customerID, cb){
     gateway.clientToken.generate({}, function (err, response) {
@@ -17,6 +21,13 @@ module.exports = {
     gateway.transaction.sale({
       amount: amount,
       paymentMethodNonce: nonce,
+    }, cb);
+  },
+  subscribe: function(nonce, cb){
+    gateway.subscription.create({
+      paymentMethodToken: nonce,
+      planId: "4d22",
+      neverExpires: true,
     }, cb);
   }
 }
